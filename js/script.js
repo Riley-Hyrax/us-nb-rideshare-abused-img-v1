a834cf1d8af2d11736e05870d02188fc7802aadb
Riley <riley@wearehyrax.com>
2024-09-18 10:17:22 +0200

    Added tracking scripts and js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Wait until the window is fully loaded
+window.onload = function() {
+    var loadingScreen = document.getElementById('loading-screen');
+    loadingScreen.style.display = 'none';
+};
